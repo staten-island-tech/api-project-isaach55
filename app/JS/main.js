37f0a7ea4fece2f9d752664fa5263e5832350a24
isaach55 <isaach55@nycstudents.net>
2024-11-20 12:30:51 -0500

    can log data and stuff experiment later

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import "../CSS/style.css";
 
 //https://ws.audioscrobbler.com/2.0/?method=track.search&track=video+games&api_key=YOUR_API_KEY&format=json
 
@@ -8,21 +8,76 @@ API key	c4f4cbe78d2187a1928264219a8a3bb6
 Shared secret	0cb39cd4c438b9dabd5848b304b3f552
 */
 
-const apiUrl = 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=video+games&api_key=c4f4cbe78d2187a1928264219a8a3bb6&format=json';
+const apiUrl =
+  "https://ws.audioscrobbler.com/2.0/?method=track.search&track=video+games&api_key=c4f4cbe78d2187a1928264219a8a3bb6&format=json";
 
 // Use fetch to make the API request
-fetch(apiUrl)
-  .then(response => {
+/*fetch(apiUrl)
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+      throw new Error("Network response was not ok " + response.statusText);
     }
-    return response.json();  // Parse the JSON response
+    return response.json(); // Parse the JSON response
   })
-  .then(data => {
+  .then((data) => {
     // Handle the data here
-    console.log(data);  // Log the data or process it for display
+    console.log(data); // Log the data or process it for display
     // You can access the track info in the data object, e.g., data.results.trackmatches.track
+    console.log(data.results);
   })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });*/
+
+const apiKey = "c4f4cbe78d2187a1928264219a8a3bb6"; // Replace with your Last.fm API key
+const baseUrl = "http://ws.audioscrobbler.com/2.0/"; // Last.fm API endpoint
+
+// Function to search for a song by track name
+function searchTrack(trackName) {
+  const url = `${baseUrl}?method=track.search&track=${encodeURIComponent(
+    trackName
+  )}&api_key=${apiKey}&format=json`;
+
+  // Fetching data from the Last.fm API
+  fetch(url)
+    .then((response) => response.json()) // Convert the response to JSON
+    .then((data) => {
+      if (data.results && data.results.trackmatches) {
+        const tracks = data.results.trackmatches.track;
+        if (tracks.length > 0) {
+          console.log(`Found ${tracks.length} result(s) for "${trackName}":`);
+          tracks.forEach((track) => {
+            console.log(`Track: ${track.name}`);
+            console.log(`Artist: ${track.artist}`);
+            console.log(`Album: ${track.album ? track.album : "N/A"}`);
+            console.log(`URL: ${track.url}`);
+            console.log(`Playcount: ${track.playcount}`);
+            console.log(`Listeners: ${track.listeners}`);
+            console.log(
+              `Duration: ${
+                track.duration ? track.duration / 1000 : "N/A"
+              } seconds`
+            );
+            console.log(
+              `Tags: ${
+                track.toptags
+                  ? track.toptags.tag.map((tag) => tag.name).join(", ")
+                  : "N/A"
+              }`
+            );
+            console.log("---");
+          });
+        } else {
+          console.log(`No results found for "${trackName}".`);
+        }
+      } else {
+        console.log("Error retrieving data from Last.fm API");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+// Example usage of the function to search for a song
+searchTrack("Video Games");
